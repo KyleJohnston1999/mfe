181fcd854e1bda34ad66f63db3f76e5d4f950390
Kyle <kylejohnston1999@gmail.com>
2023-06-20 14:42:45 +0200

    Fix header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
 export default () => {
     return (
     <BrowserRouter>
